a44772ec0bfde88d007471313dee992e644c7e57
Tartaglia12 <114998699+Tartaglia12@users.noreply.github.com>
2024-12-30 00:26:56 +0800

    Update Mesh.h

--- LibCarla/source/carla/geom/Mesh.h
+++ LibCarla/source/carla/geom/Mesh.h
@@ -121,23 +121,46 @@ namespace geom {
     // -- 其他方法 -------------------------------------------------------------
     // =========================================================================
 
-    const std::vector<vertex_type> &GetVertices() const;
+    // 以下是Mesh类（这里推测是与图形学中网格模型相关的类）中的一系列成员函数声明，用于获取网格模型相关的数据。
 
-    std::vector<vertex_type> &GetVertices();
+// 该函数返回一个对存储顶点数据的vector容器的常量引用。
+// 由于返回的是常量引用，调用者不能通过这个引用修改容器内的数据，常用于在不改变顶点数据的情况下获取顶点信息，
+// 比如在渲染等操作中仅仅是读取顶点数据来进行后续处理。
+const std::vector<vertex_type> &GetVertices() const;
 
-    size_t GetVerticesNum() const;
+// 该函数返回一个存储顶点数据的vector容器的引用，通过这个引用可以修改容器内的顶点数据，
+// 例如在需要动态调整网格模型顶点位置等场景下使用，调用者能够利用这个引用对顶点数据进行修改操作。
+std::vector<vertex_type> &GetVertices();
 
-    const std::vector<normal_type> &GetNormals() const;
+// 该函数返回一个size_t类型的值，表示网格模型中顶点的数量。
+// 这个函数常被用于获取顶点数量信息，比如在循环遍历所有顶点或者判断模型复杂度等相关操作时会用到。
+// 由于函数被声明为const，意味着调用该函数不会修改类的成员变量，只是获取数据。
+size_t GetVerticesNum() const;
 
-    const std::vector<index_type>& GetIndexes() const;
+// 该函数返回一个对存储法线向量数据的vector容器的常量引用，
+// 调用者通过这个引用只能读取法线向量数据，不能进行修改，常用于渲染光照计算等场景中需要获取法线信息时使用。
+const std::vector<normal_type> &GetNormals() const;
 
-    std::vector<index_type> &GetIndexes();
+// 该函数返回一个对存储索引数据的vector容器的常量引用，
+// 同样返回常量引用意味着只能读取索引数据，常用于在绘制网格模型时，按照索引来获取对应的顶点等数据进行绘制操作，
+// 保证不会意外修改索引数据。
+const std::vector<index_type>& GetIndexes() const;
 
-    size_t GetIndexesNum() const;
+// 该函数返回一个存储索引数据的vector容器的引用，通过这个引用可以对索引数据进行修改，
+// 比如在动态更新网格模型的拓扑结构等场景下，能够利用这个引用改变索引值来调整模型的结构。
+std::vector<index_type> &GetIndexes();
 
-    const std::vector<uv_type> &GetUVs() const;
+// 该函数返回一个size_t类型的值，表示网格模型中索引的数量，
+// 常用于确定索引相关的循环范围或者判断索引数据的规模等情况，且不会修改类的成员变量，因为函数是const的。
+size_t GetIndexesNum() const;
 
-    const std::vector<material_type> &GetMaterials() const;
+// 该函数返回一个对存储UV坐标数据的vector容器的常量引用，
+// 主要用于获取UV坐标信息，在纹理映射等操作中，读取UV坐标来确定纹理在模型表面的映射位置，且保证数据不被修改。
+const std::vector<uv_type> &GetUVs() const;
+
+// 该函数返回一个对存储材质数据的vector容器的常量引用，
+// 可用于获取网格模型对应的材质相关信息，比如材质的颜色、纹理等属性，同样调用者不能通过这个引用修改材质数据。
+const std::vector<material_type> &GetMaterials() const;
 
     /// 返回最后添加的顶点索引（顶点数）。
     size_t GetLastVertexIndex() const;
